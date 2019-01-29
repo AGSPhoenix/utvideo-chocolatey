4fb8ff0f96eb9145a2f7f899968a47025602d022
AGSPhoenix <AGSPhoenix@users.noreply.github.com>
2019-01-28 23:47:59 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+This repository is used for updating the Chocolatey package [utvideo](https://chocolatey.org/packages/utvideo).
+
+Appveyor config is in appveyor.yml, chocolatey stuff is in the nuspec.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'utvideo'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://umezawa.dyndns.info/archive/utvideo/utvideo-20.1.0-win.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+
+  softwareName  = 'Ut Video Codec Suite'
+  checksum      = '2960645D8062038D3DC007C1378CC6E595FE9563FD82167AC33E9CC6A8EB6BE3'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
