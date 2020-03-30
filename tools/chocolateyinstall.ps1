88f9c901d1eacefaccc49a3ebeb1f1d0cf6f2bb8
AGSPhoenix <AGSPhoenix@users.noreply.github.com>
2020-03-29 20:24:21 -0700

    20.6.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'utvideo'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://umezawa.dyndns.info/archive/utvideo/utvideo-20.6.0-win.exe'
+$url        = 'http://umezawa.dyndns.info/archive/utvideo/utvideo-20.6.1-win.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes= @(0)
 
   softwareName  = 'Ut Video Codec Suite'
-  checksum      = '2AD91D0190EE4978499C84A98DFA71CB4344F3B32A8046AC9420BA274C497048'
+  checksum      = '8BDB48D2C7E6850FDAD9207688FC1E84ACFA72C26E464DA63769B3639E0A9218'
   checksumType  = 'sha256'
 }
 
