ce4c57b7eca33f8a8b5043af3fcd1136a83e7740
utvideo-chocolatey autoupdate <agsphoenix@users.noreply.github.com>
2022-05-03 17:25:19 +0000

    Automatic update

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'utvideo'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/umezawatakeshi/utvideo/releases/download/utvideo-23.0.0/utvideo-23.0.0-win.exe'
+$url        = 'https://github.com/umezawatakeshi/utvideo/releases/download/utvideo-23.0.1/utvideo-23.0.1-win.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes= @(0)
 
   softwareName  = 'Ut Video Codec Suite'
-  checksum      = '8312DB0F6CE041D4C2F12883CD0DEF28394045BD01E747E01204132C4D8993DD'
+  checksum      = '50D4EDEFBC7D8A3C577BE7FE5789E9AEB122D59EADA1A6164487B31ECB109313'
   checksumType  = 'sha256'
 }
 
